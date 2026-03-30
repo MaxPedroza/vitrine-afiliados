c3f6815ee95af57e0313436e609d4405e522697c
MaxwellPedroza <seu-email@example.com>
2026-03-29 22:06:09 -0300

    Auto-generated commit

---
+++ .angular/cache/20.3.21/vitrine-afiliados/vite/deps/@angular_fire_compat.js
@@ -0,0 +1,29 @@
+import {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+} from "./chunk-OOOQIHOX.js";
+import "./chunk-I2VGT6WW.js";
+import "./chunk-SWUWZ3GL.js";
+import "./chunk-DN7ZBRWY.js";
+import "./chunk-X7ZBLB4S.js";
+import "./chunk-N2G3NVBP.js";
+import "./chunk-7X3X2PE3.js";
+import "./chunk-ZNVZ47EP.js";
+import "./chunk-B2KI3AIV.js";
+import "./chunk-35ENWJA4.js";
+export {
+  AngularFireModule,
+  FIREBASE_APP_NAME,
+  FIREBASE_OPTIONS,
+  FirebaseApp,
+  ɵapplyMixins,
+  ɵcacheInstance,
+  ɵfirebaseAppFactory,
+  ɵlazySDKProxy
+};
---
+++ chunk-VMKJLTE3.js
@@ -0,0 +1,1 @@
+import"./chunk-OCOJDHTY.js";
